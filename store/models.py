f9b576326afcec3e0e482a0804e499b80505576a
dark-glich <mohdbilal9808464636@gmail.com>
2021-03-21 20:31:25 +0530

    created

---
+++ store/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def ImageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url 
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    date_oredered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.id)
+        
+    @property
+    def get_cart_total(self):
+        items = self.orderitem_set.all()
+        total = sum([item.get_total for item in items])
+        return total
+
+    
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL,null=True)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+    quantity = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True) 
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+
+class ShippingAdress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    adrress = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True) 
+
+    def __str__(self):
+        return f'{self.adrress}, {self.city}, {self.zipcode}'
+
---
+++ store/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from .models import *
+
+def store(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'store/store.html', context)
+
+def cart(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, complete = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        total_items = 0
+        for item in items:
+            total_items += 1
+    else:
+        items = []
+        order = {'get_cart_total': 00.00}
+        total_items = 0 
+
+    context = {'items':items, 'order': order, 'total_items':total_items}
+    return render(request, 'store/cart.html', context)
+
+def checkout(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, complete = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        total_items = 0
+        for item in items:
+            total_items += 1
+    else:
+        items = []
+        order = {'get_cart_total': 00.00}
+        total_items = 0 
+
+    context = {'items':items, 'order': order, 'total_items':total_items}
+    return render(request, 'store/checkout.html', context)
