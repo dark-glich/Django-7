e67b78961dae4d3ea5d5ad032f5d560e2cf359bd
dark-glich <mohdbilal9808464636@gmail.com>
2021-05-04 15:36:15 +0530

    created add to cart feature

--- src/static/javascript/main.js
+++ src/static/javascript/main.js
@@ -1,9 +1,32 @@
-var updateBtn = document.getElementsByClassName("update-button")
+let updateBtn = document.getElementsByClassName("update-button")
 
-for(var i = 0; i < updateBtn.length; i++){
+for(let i = 0; i < updateBtn.length; i++){
     updateBtn[i].addEventListener('click', function(){
-        var productId = this.dataset.product
-        var action = this.dataset.action
+        let productId = this.dataset.product
+        let action = this.dataset.action
         console.log(productId, action)
+        if (user == 'AnonymousUser'){
+            console.log('Not Authenticared User')
+        }else{
+            Posttoken(productId, action)
+        }
+    })
+
+}function Posttoken(productId, action) {
+    console.log(`Authenticared User : ${user}`)
+    let url = '/update_item/'
+    fetch(url,{
+        method:'POST',
+        headers:{
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken
+        },
+        body: JSON.stringify({'productId':productId, 'action':action})
+    })
+    .then((response) =>{
+        return response.json()
+    })
+    .then((data) =>{
+        console.log(data)
     })
 }
--- src/store/views.py
+++ src/store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import *
+from django.http import JsonResponse
+import json
 
 def store(request):
     products = Product.objects.all()
@@ -37,3 +39,25 @@ def checkout(request):
 
     context = {'items':items, 'order': order, 'total_items':total_items}
     return render(request, 'store/checkout.html', context)
+
+
+def UpdateItem(request):
+    data = json.loads(request.body.decode("utf-8"))
+    productID = data['productId']
+    action = data['action']
+    customer = request.user.customer
+    product = Product.objects.get(id=productID)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    orderitem, created = OrderItem.objects.get_or_create(order=order, product=product)
+    
+    if action == 'add':
+        orderitem.quantity += 1
+    elif action == 'remove':
+        orderitem.quantity -= 1
+    
+    orderitem.save()
+    
+    if orderitem.quantity <= 0:
+        orderitem.delete()
+
+    return JsonResponse('Item Added', safe=False)
